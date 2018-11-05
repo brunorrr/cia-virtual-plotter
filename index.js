c252aaeed1a54edc8aefc4ab03302adc8d7b0930
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-11-04 22:57:12 -0200

    Change index.js structure

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 'use strict';
 
-import rplFinder from './rpl-finder';
-import parser from './parser';
+import { findRpl, findDaily } from './finder';
+import { parseRpl, parseDaily } from './parser';
 
 export default callback => {
-  rplFinder(null, dataArray => parser(dataArray, flights => callback(flights) ) ); 
+  findDaily((header, lines) =>
+    findRpl(
+      null,
+      rpl => parseRpl(
+        rpl,
+        rplFlights => {
+          const dailyArray = parseDaily(header, lines);
+          callback(dailyArray.map(value => {
+            const fullFlightId = value.compCode.concat(value.flightNumber.replace(/^0+/, ''));
+            for (let flight in rplFlights) {
+              if (flight.flightNumber === fullFlightId) {
+                return flight;
+              }
+            }
+            // TODO returning Daily data
+          }));
+        }
+      )
+    )
+  );
 };
