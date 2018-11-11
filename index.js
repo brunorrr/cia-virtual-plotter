0c6932b4e293207b298a95e3e70b3e701a1f7db3
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-11-10 23:45:23 -0200

    Add non RPL flights to return

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import { findRpl, findDaily } from './finder';
 import { parseRpl, parseDaily } from './parser';
+import moment from 'moment';
 
 export default callback => {
   findDaily((header, lines) =>
@@ -13,12 +14,34 @@ export default callback => {
           const dailyArray = parseDaily(header, lines);
           callback(dailyArray.map(value => {
             const fullFlightId = value.compCode.concat(value.flightNumber.replace(/^0+/, ''));
-            for (let flight in rplFlights) {
-              if (flight.flightNumber === fullFlightId) {
+            for (let flight of rplFlights) {
+              if (flight.flightNumber == fullFlightId) {
                 return flight;
               }
             }
-            // TODO returning Daily data
+            return {
+              initialDate: moment(value.operationStartingDate, 'YYYY-MM-DD').toDate(),
+              finalDate: moment(value.operationEndingDate, 'YYYY-MM-DD').toDate(),
+              daysOfWeek: new Set([
+                value.monday,
+                value.tuesday,
+                value.wednesday,
+                value.thursday,
+                value.friday,
+                value.saturday,
+                value.sunday
+              ].map(value => value !== '0' ? value : null)),
+              flightNumber: fullFlightId,
+              airFrame: value.airframe,
+              depAerodrome: value.departureIcao,
+              depTime: moment(value.estimatedDeparture, 'HH:mm'),
+              flightSpeed: '',
+              flightAltitude: '',
+              route: '',
+              arrAerodrome: value.estimatedArrival,
+              flightEet: '',
+              remarks: ''
+            };
           }));
         }
       )
